43a507defb9b8a11e56bbe7126f5d6f8b1122faf
Zer0cheros <christian.wiss@optimaedu.fi>
2023-02-17 12:56:13 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {auth, app, db} from './config'
 import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut} from 'firebase/auth'
-import {collection, addDoc} from 'firebase/firestore'
+import {collection, addDoc, onSnapshot} from 'firebase/firestore'
 const signIn = document.getElementById('sign')
 const provider = new GoogleAuthProvider()
 
@@ -24,3 +24,14 @@ onAuthStateChanged(auth, (user)=>{
     }
 })
 
+//hämtar från db
+onSnapshot(pokeRef, (snap)=>{
+    snap.forEach(doc=>{
+        document.getElementById('content').innerHTML += `
+        <div class="min-h-32 w-32 shadow-lg rounded-lg text-center text-bold p-3">
+            <h1>${doc.data().p.name}</h1>
+            <h2>${doc.data().p.weight}</h2>
+            <img class="bg-transparent rounded drop-shadow-xl m-auto" height="40px" width="40px" src="https://img.pokemondb.net/artwork/large/${doc.data().p.name}.jpg" >
+        </div>`
+    })
+})
